baf52fff09218a7b8f336c279d39d3b4da253371
Gorkem Pasaoglu <grkmpasaoglu@gmail.com>
2023-08-08 13:24:34 +0300

    first commit

---
+++ my-project/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Blog from "@/components/blogs";
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Blog />
+    </main>
+  );
+}
